2d9edbc66dc9a24f00b3a56667e7251f2b6b8755
Carl Kittelberger <icedream@icedream.pw>
2025-06-04 22:20:46 +0200

    Update year to 2025.

--- main.go
+++ main.go
@@ -117,7 +117,7 @@ func usage() {
 
 func main() {
 	fmt.Println("IceCon - Icedream's RCON Client")
-	fmt.Println("\t\u00A9 2016-2024 Carl Kittelberger/Icedream")
+	fmt.Println("\t\u00A9 2016-2025 Carl Kittelberger/Icedream")
 	fmt.Println()
 
 	argAddressTCP := argAddress.TCP()
